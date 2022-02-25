e2fe209096c3b17b1390dbdf8d9e56a72c42939e
wallst-code <senit@senitlutgen.com>
2022-02-24 17:03:22 -0800

    first commit

---
+++ app.js
@@ -0,0 +1,21 @@
+"use strict";
+console.log(5 ** 2 * 3 + 5);
+
+//solve for a string to a number
+
+const typeW = 5;
+const typeU = "*";
+
+const typeE = 3;
+
+const numArr = ["5", "*", "3"];
+
+const newArr = Number([...numArr]);
+console.log(newArr);
+
+function seeArg(...args) {
+  console.log(args);
+  return args;
+}
+
+seeArg(1, 2, 3, 4, 5, 6, 7, 8);

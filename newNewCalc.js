7fd939c95ecf429d39f37585c682d81b50dfeaa4
wallst-code <senit@senitlutgen.com>
2022-03-17 08:59:34 -0700

    fixed minor error

--- newNewCalc.js
+++ newNewCalc.js
@@ -1,4 +1,4 @@
-"use strict";
+'use strict';
 let result;
 let memory = []; // inputs and results - to make a tape and memory
 let stringNumber = [];
@@ -6,103 +6,103 @@ let operand1;
 let operand2;
 let operator;
 
-const buttons = document.getElementsByClassName("btn");
-const numberButtons = document.getElementsByClassName("btn");
-const btn1 = document.getElementById("btn-id-1");
-const btn2 = document.getElementById("btn-id-2");
-const btn3 = document.getElementById("btn-id-3");
-const btn4 = document.getElementById("btn-id-4");
-const btn5 = document.getElementById("btn-id-5");
-const btn6 = document.getElementById("btn-id-6");
-const btn7 = document.getElementById("btn-id-7");
-const btn8 = document.getElementById("btn-id-8");
-const btn9 = document.getElementById("btn-id-9");
-const btnZero = document.getElementById("btn-id-0");
-const btnClear = document.getElementById("btn-id-clear");
-// const btnDelete = document.getElementById("btn-id-delete");
-const btnAdd = document.getElementById("btn-id-addition");
-const btnSubtract = document.getElementById("btn-id-subtract");
-const btnMultiply = document.getElementById("btn-id-multiply");
-const btnDivide = document.getElementById("btn-id-divide");
-const btnDecimal = document.getElementById("btn-id-decimal");
-const btnEqual = document.getElementById("btn-id-equal");
-const display = document.getElementsByClassName("display");
+const buttons = document.getElementsByClassName('btn');
+const numberButtons = document.getElementsByClassName('btn');
+const btn1 = document.getElementById('btn-id-1');
+const btn2 = document.getElementById('btn-id-2');
+const btn3 = document.getElementById('btn-id-3');
+const btn4 = document.getElementById('btn-id-4');
+const btn5 = document.getElementById('btn-id-5');
+const btn6 = document.getElementById('btn-id-6');
+const btn7 = document.getElementById('btn-id-7');
+const btn8 = document.getElementById('btn-id-8');
+const btn9 = document.getElementById('btn-id-9');
+const btnZero = document.getElementById('btn-id-0');
+const btnClear = document.getElementById('btn-id-clear');
+const btnAdd = document.getElementById('btn-id-addition');
+const btnSubtract = document.getElementById('btn-id-subtract');
+const btnMultiply = document.getElementById('btn-id-multiply');
+const btnDivide = document.getElementById('btn-id-divide');
+const btnDecimal = document.getElementById('btn-id-decimal');
+const btnEqual = document.getElementById('btn-id-equal');
+const display = document.getElementsByClassName('display');
 
 ////
-btn1.addEventListener("click", function (e) {
+btn1.addEventListener('click', function (e) {
   operandBuilder(e);
 });
-btn2.addEventListener("click", function (e) {
+btn2.addEventListener('click', function (e) {
   operandBuilder(e);
 });
-btn3.addEventListener("click", function (e) {
+btn3.addEventListener('click', function (e) {
   operandBuilder(e);
 });
-btn4.addEventListener("click", function (e) {
+btn4.addEventListener('click', function (e) {
   operandBuilder(e);
 });
-btn5.addEventListener("click", function (e) {
+btn5.addEventListener('click', function (e) {
   operandBuilder(e);
 });
-btn6.addEventListener("click", function (e) {
+btn6.addEventListener('click', function (e) {
   operandBuilder(e);
 });
-btn7.addEventListener("click", function (e) {
+btn7.addEventListener('click', function (e) {
   operandBuilder(e);
 });
-btn8.addEventListener("click", function (e) {
+btn8.addEventListener('click', function (e) {
   operandBuilder(e);
 });
-btn9.addEventListener("click", function (e) {
-  operandBuilder(e);
-});
-
-btnZero.addEventListener("click", function (e) {
+btn9.addEventListener('click', function (e) {
   operandBuilder(e);
 });
 
-btnAdd.addEventListener("click", function (e) {
-    operationController(e);
+btnZero.addEventListener('click', function (e) {
+  operandBuilder(e);
+});
 
-  btnSubtract.addEventListener("click", function (e) {
-    operationController(e);
-  });
+btnAdd.addEventListener('click', function (e) {
+  operationController(e);
+});
 
-  btnMultiply.addEventListener("click", function (e) {
-    operationController(e);
-  });
+btnSubtract.addEventListener('click', function (e) {
+  operationController(e);
+});
 
-  btnDivide.addEventListener("click", function (e) {
-    operationController(e);
-  });
+btnMultiply.addEventListener('click', function (e) {
+  operationController(e);
+});
 
-  btnClear.addEventListener("click", function (e) {
-    clearCalculator(e);
-  }); 
+btnDivide.addEventListener('click', function (e) {
+  operationController(e);
+});
 
-  btnDecimal.addEventListener("click", function (e) {
-    decimalController(e);
-  });
+btnClear.addEventListener('click', function (e) {
+  clearCalculator(e);
+});
 
-  btnEqual.addEventListener("click", function (e) {
-    operationController(e);
-  });
+btnDecimal.addEventListener('click', function (e) {
+  decimalController(e);
+});
 
-  function operandBuilder(e) {
-    let str = e.target.textContent;
-    stringNumber += str;
-    console.log("str =", str);
-    console.log("string number =", stringNumber);
-  }
+btnEqual.addEventListener('click', function (e) {
+  operationController(e);
+});
 
-  function operationController(e){
-    console.log(operation, 'operation controller');
+function operandBuilder(e) {
+  let str = e.target.textContent;
+  stringNumber += str;
+  console.log('str =', str);
+  console.log('string number =', stringNumber);
 }
 
-function decimalController(e){
-    console.log('decimal controller');
+function operationController(e) {
+  console.log(operation, 'operation controller');
+}
+
+function decimalController(e) {
+  console.log('decimal controller');
 }
 
 function clearCalculator(e) {
-    console.log('clear');
-};
+  console.log('clear');
+}

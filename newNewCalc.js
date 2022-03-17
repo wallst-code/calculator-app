da7eb8b751954bbf223782ce805242a1e776f791
wallst-code <senit@senitlutgen.com>
2022-03-17 09:59:03 -0700

    nwe new calc

--- newNewCalc.js
+++ newNewCalc.js
@@ -5,6 +5,7 @@ let stringNumber = [];
 let operand1;
 let operand2;
 let operator;
+let operationsCounter = 0;
 
 const buttons = document.getElementsByClassName('btn');
 const numberButtons = document.getElementsByClassName('btn');
@@ -61,19 +62,19 @@ btnZero.addEventListener('click', function (e) {
 });
 
 btnAdd.addEventListener('click', function (e) {
-  operationController(e);
+  calcController(e);
 });
 
 btnSubtract.addEventListener('click', function (e) {
-  operationController(e);
+  calcController(e);
 });
 
 btnMultiply.addEventListener('click', function (e) {
-  operationController(e);
+  calcController(e);
 });
 
 btnDivide.addEventListener('click', function (e) {
-  operationController(e);
+  calcController(e);
 });
 
 btnClear.addEventListener('click', function (e) {
@@ -85,24 +86,91 @@ btnDecimal.addEventListener('click', function (e) {
 });
 
 btnEqual.addEventListener('click', function (e) {
-  operationController(e);
+  calcController(e);
 });
 
 function operandBuilder(e) {
   let str = e.target.textContent;
   stringNumber += str;
-  console.log('str =', str);
+  str = '';
   console.log('string number =', stringNumber);
 }
 
-function operationController(e) {
-  console.log(operation, 'operation controller');
+function calcController(e) {
+  console.log(' calc controller', e.target.textContent);
+  operator = e.target.textContent;
+  operationsCounter++;
+  console.log('counter is at', operationsCounter);
+
+  // if new calc - it may be easier to check the number of operators
+  if (operationsCounter === 1 && operand1 === undefined) {
+    console.log('new calc operation');
+    firstCalculation();
+  } else if (operationsCounter >= 2) {
+    console.log('cont calc - greater than 2');
+    continuedCalculations();
+  } else {
+    console.log('continuous calc');
+  }
+
+  // lots of if then logic here.
 }
 
 function decimalController(e) {
-  console.log('decimal controller');
+  console.log('decimal controller', e.target.textContent);
+  // can only use once per operand
 }
 
 function clearCalculator(e) {
   console.log('clear');
+  // clear display and operands, operators
+}
+
+function calcMemory(something) {
+  console.log('memory');
+}
+
+function firstCalculation() {
+  console.log('first calc function');
+  operand1 = Number(stringNumber);
+  stringNumber = [];
+  console.log('operand1 =', operand1);
+  console.log('stringNumber should be cleared from 1st Calc', stringNumber);
+}
+
+function continuedCalculations() {
+  console.log('cont calc function');
+  operand2 = Number(stringNumber);
+  stringNumber = [];
+  console.log('stringNumber Cleared in contCalc', stringNumber);
+  console.log('operand2 = ', operand2);
+  result = calcFunction(operand1, operator, operand2);
+  displayResult(result);
+}
+
+function displayResult(result) {
+  console.log('result display function');
+  console.log(result);
+}
+
+function calcFunction(num1, operator, num2) {
+  console.log('calc funciton running');
+  if (operator === '+') {
+    result = num1 + num2;
+    return result;
+  } else if (operator === '-') {
+    result = num1 - num2;
+    return result;
+  } else if (operator === 'x') {
+    result = num1 * num2;
+    return result;
+  } else if (operator === '/') {
+    if (num2 === 0) {
+      console.error('Error cannot divide by 0');
+      return undefined;
+    } else {
+      result = num1 / num2;
+      return result;
+    }
+  }
 }
